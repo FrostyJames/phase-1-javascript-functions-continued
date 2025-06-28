878659ccf47818588ac130fcbf94f4e98d9b09bb
james <ivanlavan773@gmail.com>
2025-06-28 11:48:38 +0300

    Updated js file

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function wrapAdjective(flair = "*") {
     return `You are ${flair}${adjective}${flair}!`;
   };
 }
+
+//console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+//console.log(mondayWork())
+//console.log(wrapAdjective("||")("a hard worker")); // "You are ||a hard worker||!"
